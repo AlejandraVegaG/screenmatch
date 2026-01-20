bf29ba0a744f4c93af603e7cc8b62b7e141bebb2
AlejandraVegaG <alejandravegag01@gmail.com>
2026-01-19 21:32:08 -0600

    Refactorizado a Principal, interacción con el usuario usando Scanner

--- src/main/java/com/alevg/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/alevg/screenmatch/ScreenmatchApplication.java
@@ -3,6 +3,7 @@ package com.alevg.screenmatch;
 import com.alevg.screenmatch.model.DatosOmdEpisodio;
 import com.alevg.screenmatch.model.DatosOmdTemporada;
 import com.alevg.screenmatch.model.DatosOmdbSerie;
+import com.alevg.screenmatch.principal.Principal;
 import com.alevg.screenmatch.service.ConsumoAPI;
 import com.alevg.screenmatch.service.ConvierteDatos;
 import org.springframework.boot.CommandLineRunner;
@@ -23,36 +24,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Hello there!");
-
-        Properties props = new Properties();
-
-        props.load(new FileInputStream("config.properties"));
-        String API_KEY = props.getProperty("API_KEY");
-
-        var consumoApi = new ConsumoAPI();
-        var json = consumoApi.obtenerDatos("http://www.omdbapi.com/?t=icarly&apikey="+API_KEY);
-        System.out.println(json);
-
-        ConvierteDatos conversorDatos = new ConvierteDatos();
-        var datos = conversorDatos.obtenerDatos(json, DatosOmdbSerie.class);
-        System.out.println(datos);
-
-        // Episodio
-        json = consumoApi.obtenerDatos("http://www.omdbapi.com/?t=icarly&Season=1&episode=1&apikey="+API_KEY);
-        var datosEpisodios = conversorDatos.obtenerDatos(json, DatosOmdEpisodio.class);
-
-        System.out.println(datosEpisodios);
-
-        // Temporadas
-        List<DatosOmdTemporada> temporadas = new ArrayList<>();
-        for (int i = 1; i <= datos.totalTemporadas(); i++) { //datos es de la serie
-            json = consumoApi.obtenerDatos("http://www.omdbapi.com/?t=icarly&Season=" + i + "&apikey=" + API_KEY);
-            var datosTemporadas = conversorDatos.obtenerDatos(json, DatosOmdTemporada.class);
-            temporadas.add(datosTemporadas);
-        }
-        temporadas.forEach(System.out::println);
-
-
+        Principal principal = new Principal();
+        principal.muestraMenu();
     }
 }
---
+++ src/main/java/com/alevg/screenmatch/principal/Principal.java
@@ -0,0 +1,47 @@
+package com.alevg.screenmatch.principal;
+
+import com.alevg.screenmatch.model.DatosOmdTemporada;
+import com.alevg.screenmatch.model.DatosOmdbSerie;
+import com.alevg.screenmatch.service.ConsumoAPI;
+import com.alevg.screenmatch.service.ConvierteDatos;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.Scanner;
+
+public class Principal {
+    private Scanner scanner = new Scanner(System.in);
+    private final String URL_API_OMDB = "http://www.omdbapi.com/?t=";
+    private String API_KEY = "&apikey=";
+    private ConvierteDatos conversor = new ConvierteDatos();
+
+    public void muestraMenu() throws IOException {
+        // Recibir nombre de la serie
+        System.out.println("Escriba el nombre de la serie a buscar: ");
+        var nombreSerie = scanner.nextLine();
+
+        // Obtener la API Key
+        Properties props = new Properties();
+        props.load(new FileInputStream("config.properties"));
+        this.API_KEY = this.API_KEY +  props.getProperty("API_KEY") ;
+
+        // Llamar a la API OMDB
+        var consumoApi = new ConsumoAPI();
+
+        // Obtener datos de la serie
+        var json = consumoApi.obtenerDatos(this.URL_API_OMDB + nombreSerie.replace(" ", "+") + this.API_KEY);
+        var datos = conversor.obtenerDatos(json, DatosOmdbSerie.class);
+        // Obtener todas las temporadas de la serie
+        List<DatosOmdTemporada> temporadas = new ArrayList<>();
+        for (int i = 1; i <= datos.totalTemporadas(); i++) { //datos es de la serie
+            json = consumoApi.obtenerDatos(this.URL_API_OMDB + nombreSerie.replace(" ", "+") + "&Season=" + i + API_KEY);
+            var datosTemporadas = conversor.obtenerDatos(json, DatosOmdTemporada.class);
+            temporadas.add(datosTemporadas);
+        }
+        temporadas.forEach(System.out::println);
+    }
+}
